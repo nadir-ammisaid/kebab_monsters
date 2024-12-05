670ebf58bd947e981277af7a37d8440fc23370d8
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-12-05 17:23:18 +0100

    conflit merge

--- client/src/components/Maps.tsx
+++ client/src/components/Maps.tsx
@@ -10,11 +10,11 @@ export default function Maps() {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			{kebabs.map((kebab) => (
-				<Marker key={kebab.id} position={[51.505, -0.09]}>
-					<Popup>This is a popup</Popup>
-				</Marker>
-			))}
+			{/* {kebabs.map((kebab) => ( */}
+			<Marker position={[51.505, -0.09]}>
+				<Popup>This is a popup</Popup>
+			</Marker>
+			{/* ))} */}
 		</MapContainer>
 	);
 }
