6f323fb2b111de09e682793250147271c010af73
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-12-05 14:20:19 +0100

    ✨ : button route Homepage

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,5 +1,17 @@
+import "./styles/welcome.css";
+import logo from "../assets/images/DALL·E 2024-12-05 10.32.35 - Create a logo design for 'Kebab Monsters', a fun and quirky brand. The logo should feature a cartoon-style kebab with a playful monster face, includin.webp"
+import { Link } from "react-router-dom";
+
 export default function Welcome() {
     return (
-        <div>Welcome</div>
+        <>
+            <div className="welcome_center">
+                <img src={logo} alt="logo" />
+                <p>Hello, Hello, Kebab enjoyers</p>
+                <Link to="/">
+                    <button type="button">Start</button>
+                </Link>
+            </div>
+        </>
     )
 }
