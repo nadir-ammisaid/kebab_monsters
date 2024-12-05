8e5d61053ec274bb30a58b9be97519ca9a8f70ab
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-12-05 17:11:15 +0100

    relier map avec context

--- client/src/components/map.tsx
+++ client/src/components/map.tsx
@@ -1,19 +1,20 @@
-import { useContext } from "react";
 import "../App.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import KebabContext from "../contexts/KebabContext";
+import { useKebab } from "../contexts/KebabContext";
 
 export default function Maps() {
-	const [{ kebabs }] = useContext(KebabContext);
+	const { kebabs } = useKebab();
 	return (
 		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={[51.505, -0.09]}>
-				<Popup>This is a popup</Popup>
-			</Marker>
+			{kebabs.map((kebab) => (
+				<Marker key={kebab.id} position={[51.505, -0.09]}>
+					<Popup>This is a popup</Popup>
+				</Marker>
+			))}
 		</MapContainer>
 	);
 }
