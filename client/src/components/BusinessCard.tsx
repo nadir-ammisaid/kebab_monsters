53c513d4bd8d10e6732a3f327148b246be981002
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-12-06 12:07:16 +0100

    détail dans le Kebab et page Details

--- client/src/components/BusinessCard.tsx
+++ client/src/components/BusinessCard.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import KebabContext from "../contexts/KebabContext";
 import type { KebabContextType } from "../contexts/KebabContext";
 import "./BusinessCard.css";
+import { Link } from "react-router-dom";
 
 function BusinessCard() {
 	const kebabsData = useContext(KebabContext) as KebabContextType;
@@ -18,28 +19,25 @@ function BusinessCard() {
 	return (
 		<section>
 			{filteredKebabs?.map((business) => (
-				<div key={business.id} className="businessCard">
-					<h3>{business.name}</h3>
-					<p key={business.id}> Téléphone: {business.display_phone}</p>
-					<p key={business.id}>
-						{""}
-						Rating: {business.rating || "Non noté"}
-					</p>
-					<p key={business.id}>
-						{""}
-						Adresse:{" "}
-						{business.location.display_address || "Aucune adresse renseignée"}
-					</p>
+				<Link key={business.id} to={`/details/${business.id}`}>
+					<div key={business.id} className="businessCard">
+						<h3>{business.name}</h3>
 
-					<img
-						src={
-							business.image_url
-								? business.image_url
-								: "https://www.mondialkebab.com/wp-content/uploads/2022/02/manger-un-kebab-c-est-bien-mais-avec-moderation.jpg"
-						}
-						alt={business.name}
-					/>
-				</div>
+						<p key={business.id}>
+							{""}
+							Rating: {business.rating || "Non noté"}
+						</p>
+
+						<img
+							src={
+								business.image_url
+									? business.image_url
+									: "https://www.mondialkebab.com/wp-content/uploads/2022/02/manger-un-kebab-c-est-bien-mais-avec-moderation.jpg"
+							}
+							alt={business.name}
+						/>
+					</div>
+				</Link>
 			))}
 		</section>
 	);
