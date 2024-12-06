9808eb6cb4006bf611862be27a43071d58d82b88
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-12-06 11:59:26 +0100

    détail dans le Kebab

--- client/src/components/BusinessCard.tsx
+++ client/src/components/BusinessCard.tsx
@@ -20,6 +20,17 @@ function BusinessCard() {
 			{filteredKebabs?.map((business) => (
 				<div key={business.id} className="businessCard">
 					<h3>{business.name}</h3>
+					<p key={business.id}> Téléphone: {business.display_phone}</p>
+					<p key={business.id}>
+						{""}
+						Rating: {business.rating || "Non noté"}
+					</p>
+					<p key={business.id}>
+						{""}
+						Adresse:{" "}
+						{business.location.display_address || "Aucune adresse renseignée"}
+					</p>
+
 					<img
 						src={
 							business.image_url
