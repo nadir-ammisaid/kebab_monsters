a19cf1789b58e36f48aa08d15a573c09a689250e
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-12-06 10:46:28 +0100

    map fonctionelle

--- client/src/components/Maps.tsx
+++ client/src/components/Maps.tsx
@@ -1,15 +1,42 @@
+import { useContext } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import KebabContext from "../contexts/KebabContext";
+import type { KebabContextType } from "../contexts/KebabContext";
+import "../App.css";
 
 export default function Maps() {
+	const kebabsData = useContext(KebabContext) as KebabContextType;
+
+	// Vérifiez si les données sont correctement reçues
+	console.log("Kebabs data:", kebabsData);
+
+	// Si le contexte est mal initialisé, retournez un message de chargement
+	if (!kebabsData) {
+		return <p>Chargement des données...</p>;
+	}
+
+	const { kebabs } = kebabsData;
+
 	return (
-		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+		<MapContainer
+			center={[48.8645158, 2.3454019]}
+			zoom={13}
+			scrollWheelZoom={true}
+		>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={[51.505, -0.09]}>
-				<Popup>This is a popup</Popup>
-			</Marker>
+			{kebabs.businesses.map((kebab) => (
+				<Marker
+					key={kebab.id}
+					position={[kebab.coordinates.latitude, kebab.coordinates.longitude]}
+				>
+					<Popup>
+						<strong>Kebab:</strong> {kebab.name}
+					</Popup>
+				</Marker>
+			))}
 		</MapContainer>
 	);
 }
